9bd9a04faf7c5edecc8497b8a48f86ff0818ff78
Tushar Tyagi <142096878+Tushar2401-Tyagi@users.noreply.github.com>
2025-04-23 12:44:19 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1122334455;
+let accountEmail = "tushar@gmail.com";
+var accountPassword = "12345";
+
+//This is not correct the word it is bad coding in this use so plz not be used
+accountCity = "Saharanpur"
+
+let accountState;
+
+accountEmail = "tyagi@gmail.com"
+accountPassword = "222222"
+
+// Prefer not to use var becaue of issue in block scope and functional scope.
+
+console.log([accountId, accountEmail, accountPassword, accountCity, accountState])
