450d0be284d9b7817091faa6e83b7846c6f10e46
Tushar Tyagi <142096878+Tushar2401-Tyagi@users.noreply.github.com>
2025-04-24 09:32:52 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,38 @@
+ "use strict"; // treat all js code as newer version
+
+ //alert(3 + 3); // we are using nodejs, not browser
+
+ // code readability should be high
+
+ let name = "Tushar"; // String
+
+ let age = 25; // Number
+
+ let isLoggedIn = true; // Boolean
+
+ // number => 2 to power 52
+
+ let bigInt = 123455555555555555555555; // when we use bigInt Number is also long
+ // Permitive DataTypes
+
+ // number => 25
+ // bigInt => 123456789123456789
+ // String => ""/''
+ // boolean => true/false
+ // null => null is a standalonevalue
+ // undefined => vale is not define
+ let state; //(example of undefine value)
+ // symbol => unique
+
+
+ // Object
+
+ console.log(typeof "Tushar");
+ console.log(typeof 3);
+ console.log(typeof null); //this vale output is Object
+ console.log(typeof undefined); //this vale output is undefined
+
+
+
+
+
