a372e261ea56f85313b2635e9e92d45acb3a4e31
Tushar Tyagi <142096878+Tushar2401-Tyagi@users.noreply.github.com>
2025-04-24 16:13:17 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -7,6 +7,9 @@
 // false => 0
 // null => 0
 
+// visit ECMAScript site
+// visit Mdn
+
 
 //console.log(score);
 // console.log(typeof score);
@@ -15,10 +18,10 @@
 // console.log(valueInNumber);
 // console.log(typeof valueInNumber);
 
-let isLoggedIn = 0;
+// let isLoggedIn = 0;
 
-let booleanIsLoggedIn = Boolean(isLoggedIn); // convert number to boolean
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn); // convert number to boolean
+// console.log(booleanIsLoggedIn);
 
 // 1 => true
 // 0 => false
@@ -26,7 +29,42 @@ console.log(booleanIsLoggedIn);
 // "abc" => true
 // null => false
 
-let someNumber = 33;
-let stringNumber = String(someNumber); // convert number to string
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33;
+// let stringNumber = String(someNumber); // convert number to string
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+/*******************Operations************************* */
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2) // add
+// console.log(2-2) //sub
+// console.log(2*2) //mul
+// console.log(2**2) //power
+// console.log(2/2) //div
+// console.log(2%2) //modulus
+
+// let str1 = 3;
+// let str2 = 10;
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(1 + "2" + 2);
+
+// console.log(true);
+// console.log(+true);
+// console.log(+false);
+// //console.log(++true);//unexceptable into the javascript
+//console.log(+"");
+
+let gameCounter = 100;
+gameCounter++;//prefix
+++gameCounter;//postfix
+console.log(gameCounter);
